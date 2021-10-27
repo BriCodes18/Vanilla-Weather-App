ebc3b0bae7b69f1836709ab5b48e6f393faad21d
BriCodes18 <91579291+BriCodes18@users.noreply.github.com>
2021-10-26 19:01:43 -0700

    Included Event Listener

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ function updateTemperature(response) {
   console.log(response.data);
 }
 
-let apiKey = "b190b9864cf7bf2f14432317f03ad0e6";
-let units = "imperial";
-let city = `London`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+function getTemperature(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city").value;
 
-axios.get(apiUrl).then(updateTemperature);
+  let apiKey = "b190b9864cf7bf2f14432317f03ad0e6";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(updateTemperature);
+}
+
+let weatherForm = document.querySelector("#city");
+weatherForm.addEventListener("submit", getTemperature);
