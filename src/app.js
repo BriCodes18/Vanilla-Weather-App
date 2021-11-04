7b028c70a9ef84f3ff2971fd6f8dff579de7bb1c
BriCodes18 <91579291+BriCodes18@users.noreply.github.com>
2021-11-03 21:50:50 -0700

    Integrate Forecast API

--- src/app.js
+++ src/app.js
@@ -25,31 +25,48 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function showForecast(response) {
-  console.log(response);
-  let forecastElement = document.querySelector("#forecast");
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
 
-  let days = ["Thurs", "Fri", "Sat", "Sun", "Mon", "Tues"];
+  let day = date.getDay();
+
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function showForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `  
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `  
             <div class="col-2">
-              <div class="forecast-date">${day}</div>
+              <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
               <img
-                src="https://openweathermap.org/img/wn/01d@2x.png"
+                src="https://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 alt="Weather Icon"
                 width="48"
               />
               <div class="forecast-temps">
-                <span class="forecast-max">56°</span>
-                <span class="forecast-min">34°</span>
+                <span class="forecast-max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="forecast-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
               </div>
             </div>
           `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
